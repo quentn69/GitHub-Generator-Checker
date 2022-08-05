ff91d519d0423b48ab552dd9146264c872987e7f
quentn69 <107768845+quentn69@users.noreply.github.com>
2022-08-05 15:49:50 +0200

    .

--- GitGen.py
+++ GitGen.py
@@ -1,5 +1,5 @@
 webhook = "" # paste your webhook between the ""
-length = 3 # change this for a different length
+length = 4 # change this for a different length
 
 #===================================================================================
 
